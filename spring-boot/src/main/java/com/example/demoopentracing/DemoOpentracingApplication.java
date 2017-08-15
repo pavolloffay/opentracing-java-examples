39f51f25894e148336b74e7e69914b08ccd7c2ef
Pavol Loffay <ploffay@redhat.com>
2017-08-15 15:23:59 +0200

    Change zipkin configuration to 128bit traces and JSON encoding

--- spring-boot/src/main/java/com/example/demoopentracing/DemoOpentracingApplication.java
+++ spring-boot/src/main/java/com/example/demoopentracing/DemoOpentracingApplication.java
@@ -11,8 +11,10 @@ import com.uber.jaeger.samplers.ProbabilisticSampler;
 
 import brave.Tracing;
 import brave.opentracing.BraveTracer;
+import brave.sampler.Sampler;
 import zipkin.Span;
 import zipkin.reporter.AsyncReporter;
+import zipkin.reporter.Encoding;
 import zipkin.reporter.okhttp3.OkHttpSender;
 
 @SpringBootApplication
@@ -32,9 +34,17 @@ public class DemoOpentracingApplication {
 
 	@Bean
 	public io.opentracing.Tracer zipkinTracer() {
-		OkHttpSender okHttpSender = OkHttpSender.create("http://localhost:9411/api/v1/spans");
+		OkHttpSender okHttpSender = OkHttpSender.builder()
+				.encoding(Encoding.JSON)
+				.endpoint("http://localhost:9411/api/v1/spans")
+				.build();
 		AsyncReporter<Span> reporter = AsyncReporter.builder(okHttpSender).build();
-		Tracing braveTracer = Tracing.newBuilder().localServiceName("spring-boot").reporter(reporter).build();
+		Tracing braveTracer = Tracing.newBuilder()
+				.localServiceName("spring-boot")
+				.reporter(reporter)
+				.traceId128Bit(true)
+				.sampler(Sampler.ALWAYS_SAMPLE)
+				.build();
 		return BraveTracer.create(braveTracer);
 	}
 
